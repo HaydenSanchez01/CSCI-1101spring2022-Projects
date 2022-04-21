5a6aaf2b80e0ac4e554a308a2bc02195742d5c0f
HaydenSanchez01 <101362539+HaydenSanchez01@users.noreply.github.com>
2022-04-21 15:03:17 -0500

    Add files via upload

---
+++ games-database/sandbox-create-update-delete-queries.sql
@@ -0,0 +1,13 @@
+INSERT INTO 
+    sandbox(int16_value, double_value, datetime_value)
+VALUES 
+    (-235,1557.2467, '2095-02-15 19:28:45')
+
+UPDATE
+    sandbox
+SET int32_value = 58721649,
+    bool_value = 1 
+WHERE 
+    id = 27
+
+DELETE FROM sandbox WHERE id = 37
